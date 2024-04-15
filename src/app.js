714da85b5da8a3334cfcb550bd6bb158f53be578
Flor Molina <flormoly9@gmail.com>
2024-04-14 21:49:05 -0500

    Estructura de backend definido

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import express from 'express'
+
+//Server
+const app = express()
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import app from './app.js'
+
+
+
+
+app.listen(3000)
+console.log('Conexión establecida correctamente',3000)
+
+
